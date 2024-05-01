9d1b6efa07f97026d2169c074279dddbcb63d7f6
liangwt <320453942@qq.com>
2024-05-01 13:39:11 +0800

    提交

--- bsp/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import partCard from './partCard/index.vue'
-import searchArea from './searchArea/index.vue'
-
-const com = [partCard, searchArea]
-com.forEach((item) => {
-  Vue.component(item.name, item)
-})
--- bsp/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  userInfo: (state) => state.system.userInfo,
-  permissions: (state) => state.system.permissions,
-  menuList: (state) => state.system.menuList,
-  keepPage: (state) => state.system.keepPage
-}
-export default getters
