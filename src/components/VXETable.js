618e25bb10c50444534ae6f36e8777e13ba8b28a
liangwt <320453942@qq.com>
2024-12-24 15:34:06 +0800

    更新依赖项，替换为vxe-pc-ui并升级vxe-table，优化表格配置和格式化功能

--- src/components/VXETable.js
+++ src/components/VXETable.js
@@ -1,30 +1,50 @@
 import Vue from 'vue'
 import XEUtils from 'xe-utils'
-// import VXETable from 'vxe-table'
+import VxeUI from 'vxe-pc-ui'
+import VXETable from 'vxe-table'
 
 Vue.prototype.$vxe = XEUtils
-// Vue.prototype.$XModal = VXETable.modal
+Vue.prototype.$XModal = VXETable.modal
 
-// VXETable.formats.mixin({
-//   formatDate({ cellValue }, format) {
-//     return XEUtils.toDateString(cellValue, format || 'yyyy-MM-dd')
-//   },
-//   formatDateTime({ cellValue }, format) {
-//     return XEUtils.toDateString(cellValue, format || 'yyyy-MM-dd HH:mm:ss')
-//   },
-//   formatMoney({ cellValue }, digits) {
-//     return XEUtils.commafy(cellValue, { digits: digits || digits == 0 ? digits : 2 })
-//   },
-//   formatMeter({ cellValue }, digits) {
-//     return XEUtils.commafy(cellValue, { digits: digits || digits == 0 ? digits : 4 })
-//   }
-// })
-// VXETable.setup({
-//   table: {
-//     border: true,
-//     showOverflow: 'tooltip',
-//     autoResize: true,
-//     stripe: true
-//   }
-// })
-// Vue.use(VXETable)
+VXETable.formats.mixin({
+  formatDate ({ cellValue }, format) {
+    return XEUtils.toDateString(cellValue, format || 'yyyy-MM-dd')
+  },
+  formatDateTime ({ cellValue }, format) {
+    return XEUtils.toDateString(cellValue, format || 'yyyy-MM-dd HH:mm:ss')
+  },
+  formatMoney ({ cellValue }, digits) {
+    if (!XEUtils.isNumber(cellValue)) {
+      cellValue = Number(cellValue)
+    }
+    return XEUtils.commafy(cellValue || 0, { digits: digits || digits == 0 ? digits : 2 })
+  },
+  formatMeter ({ cellValue }, digits) {
+    if (!XEUtils.isNumber(cellValue)) {
+      cellValue = Number(cellValue)
+    }
+    return XEUtils.commafy(cellValue || 0, { digits: digits || digits == 0 ? digits : 4 })
+  }
+})
+VXETable.setConfig({
+  table: {
+    border: true,
+    showOverflow: 'tooltip',
+    autoResize: true,
+    headerAlign: 'center',
+    resizable: true,
+    rowConfig: {
+      isHover: true,
+      isCurrent: true
+    }
+    // stripe: true
+  },
+  pager: {
+    background: true,
+    pagerCount: 7,
+    pageSizes: [10, 20, 50, 100],
+    layouts: ['Total', 'Sizes', 'PrevPage', 'Number', 'NextPage', 'FullJump']
+  }
+})
+Vue.use(VxeUI)
+Vue.use(VXETable)
